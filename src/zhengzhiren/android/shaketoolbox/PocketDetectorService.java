bd8180117f6971d8fac7e4d02b81ce6233472705
tilltheendwjx@gmail.com <tilltheendwjx@gmail.com@bfed1615-f5b6-2cc9-921f-102c86f96327>
2011-11-19 04:21:36 +0000

    pocket_pattern  in preferences.xml pocketdectetor （dont shake in pocket if pocketdectetor is on） 口袋模式设置   口袋模式检测（防止手机放在口袋会触发摇晃检测）

--- src/zhengzhiren/android/shaketoolbox/PocketDetectorService.java
+++ src/zhengzhiren/android/shaketoolbox/PocketDetectorService.java
@@ -1,67 +1,102 @@
 package zhengzhiren.android.shaketoolbox;
 
-
 import java.util.List;
+
+import zhengzhiren.android.hardware.ShakeDetector;
 import zhengzhiren.android.shaketoolbox.actions.Action;
 import android.app.Service;
+import android.app.admin.DevicePolicyManager;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.content.SharedPreferences;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.IBinder;
+import android.preference.PreferenceManager;
 import android.text.format.Time;
 import android.widget.Toast;
 
-public class PocketDetectorService extends Service implements SensorEventListener {
+public class PocketDetectorService extends Service implements
+		SensorEventListener {
 
-	  private boolean mIsRegistered=false;
-	  private SensorManager mSensorMgr;
+	private boolean mIsRegistered = false;
+	private SensorManager mSensorMgr;
+	private boolean mIsPocket;
+	private static final String pocket_pattern = "pocket_pattern";
+	private final int defaultdistance = 5;
+	private SharedPreferences mSharedPrefs;
+	private ToolboxService mtoolboxservice;
+	private ServiceConnection sc = new ServiceConnection() {
+		@Override
+		public void onServiceDisconnected(ComponentName name) {
+			mtoolboxservice = null;
+		}
+
+		@Override
+		public void onServiceConnected(ComponentName name, IBinder service) {
+			mtoolboxservice = ((ToolboxService.ToolBoxServiceBinder) (service))
+					.getService();
+		}
+
+	};
+
 	public PocketDetectorService() {
 		// TODO Auto-generated constructor stub
 	}
-	private void registerProximitySensor()
-	  {
-	    SensorManager localSensorManager = this.mSensorMgr;
-	    Sensor localSensor = this.mSensorMgr.getDefaultSensor(Sensor.TYPE_PROXIMITY);
-	    localSensorManager.registerListener(this, localSensor, SensorManager.SENSOR_DELAY_NORMAL);
-	    this.mIsRegistered = true;
-	  }
-	private void unregisterProximitySensor()
-	  {
-	    this.mSensorMgr.unregisterListener(this);
-	    this.mIsRegistered = false;
-	  }
-	
+
+	private void registerProximitySensor() {
+		SensorManager localSensorManager = this.mSensorMgr;
+		Sensor localSensor = this.mSensorMgr
+				.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+		localSensorManager.registerListener(this, localSensor,
+				SensorManager.SENSOR_DELAY_NORMAL);
+		this.mIsRegistered = true;
+	}
+
+	private void unregisterProximitySensor() {
+		this.mSensorMgr.unregisterListener(this);
+		this.mIsRegistered = false;
+	}
+
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
 		// TODO Auto-generated method stub
-		List<Action> mEnabledActions= Action.getEnabledActions(this);
+		List<Action> mEnabledActions = Action.getEnabledActions(this);
 		if (mEnabledActions.isEmpty()) {
 			stopSelf();
 			return START_STICKY;
 		}
 		return START_STICKY;
 	}
+
 	@Override
 	public void onCreate() {
 		// TODO Auto-generated method stub
-		if ((!this.mIsRegistered) || (this.mSensorMgr == null))
-	    {
-	      SensorManager localSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
-	      this.mSensorMgr = localSensorManager;
-	      registerProximitySensor();
-	      //this.mDelayedStartHandler.removeCallbacksAndMessages(null);
-	    }
+		mSharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+		this.mIsPocket = mSharedPrefs.getBoolean(pocket_pattern, true);
+		Intent intent = new Intent(PocketDetectorService.this,
+				ToolboxService.class);
+		bindService(intent, sc, Context.BIND_AUTO_CREATE);
+		if ((!this.mIsRegistered) || (this.mSensorMgr == null)) {
+			SensorManager localSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+			this.mSensorMgr = localSensorManager;
+			registerProximitySensor();
+			// this.mDelayedStartHandler.removeCallbacksAndMessages(null);
+		}
 		super.onCreate();
 	}
-	
+
 	@Override
 	public void onDestroy() {
 		// TODO Auto-generated method stub
 		unregisterProximitySensor();
 		super.onDestroy();
 	}
+
 	@Override
 	public void onAccuracyChanged(Sensor arg0, int arg1) {
 		// TODO Auto-generated method stub
@@ -71,18 +106,22 @@ public class PocketDetectorService extends Service implements SensorEventListene
 	@Override
 	public void onSensorChanged(SensorEvent arg0) {
 		// TODO Auto-generated method stub
-	        if (arg0.sensor.getType() == Sensor.TYPE_PROXIMITY)
-	        {
-	          float[] arrayOfFloat = arg0.values;
-	          float j = arrayOfFloat[0];
-	          
-	          //filelog f=new filelog("pocketlog.txt");
-	          //Time t=new Time();
-	          //t.setToNow();
-	  		  //String log="pocket:"+String.valueOf(t.hour)+"/"+String.valueOf(t.minute)+"/"+String.valueOf(t.second)+"\n";
-	  		  //log+=String.valueOf(j)+"\n";
-	  		  //f.writelog(log);
-	        }
+
+		if (arg0.sensor.getType() == Sensor.TYPE_PROXIMITY) {
+			float[] arrayOfFloat = arg0.values;
+			float j = arrayOfFloat[0];
+			if (this.mIsPocket) {
+				if (j == 0 || j < this.defaultdistance) {
+					if (mtoolboxservice != null) {
+						this.mtoolboxservice.StopShakeAccelerometer();
+					}
+				} else {
+					if (mtoolboxservice != null) {
+						this.mtoolboxservice.StartShakeAccelerometer();
+					}
+				}
+			}
+		}
 	}
 
 	@Override
--- src/zhengzhiren/android/shaketoolbox/SeekBarPreference.java
+++ src/zhengzhiren/android/shaketoolbox/SeekBarPreference.java
@@ -3,71 +3,80 @@ package zhengzhiren.android.shaketoolbox;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.DialogPreference;
+import android.preference.PreferenceManager;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
 
-public class SeekBarPreference extends DialogPreference implements  OnSeekBarChangeListener {
+public class SeekBarPreference extends DialogPreference implements
+		OnSeekBarChangeListener {
 
-	private SeekBar mseekBar;  
+	private SeekBar mseekBar;
 	private TextView mtextView;
-	public final static String pre_ShakeThreshold_on_shake_value="ShakeThreshold_on_shake_value";
-	public final static int defaultvalue=5;
+	public final static String pre_ShakeThreshold_on_shake_value = "ShakeThreshold_on_shake_value";
+	public final static int defaultvalue = 5;
+	public final static int maxvalue = 10;
 	private int seekvalue;
 	private SharedPreferences mSharedPrefs;
+
 	public SeekBarPreference(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
-		 //TODO Auto-generated constructor stub
+		// TODO Auto-generated constructor stub
 	}
+
 	public SeekBarPreference(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		// TODO Auto-generated constructor stub
 	}
-	public static int Getpre_ShakeThreshold_on_shake_value(int srcthreshold)
-	{
-		return (srcthreshold+1)*500;
+
+	public static int Getpre_ShakeThreshold_on_shake_value(int srcthreshold) {
+		return (srcthreshold + 1) * 500;
 	}
-	@Override  
-    protected void onBindDialogView(View view) {  
-        // TODO Auto-generated method stub  
-        super.onBindDialogView(view);  
-        mSharedPrefs = this.getSharedPreferences();
-        seekvalue=mSharedPrefs.getInt(pre_ShakeThreshold_on_shake_value,defaultvalue);
-        mseekBar = (SeekBar) view.findViewById(R.id.seekBar);  
-        mtextView = (TextView) view.findViewById(R.id.textView);  
-        mseekBar.setOnSeekBarChangeListener(this); 
-        this.mseekBar.setProgress(this.seekvalue);
-        mtextView.setText(seekvalue + "/" + mseekBar.getMax());  
-    }  
-	 @Override  
-	protected void onDialogClosed(boolean positiveResult) {  
-	        // TODO Auto-generated method stub  
-	        if (positiveResult) {  
-	        	SharedPreferences.Editor editor = mSharedPrefs.edit();
-	        	//���ֵ
-	        	editor.putInt(pre_ShakeThreshold_on_shake_value,mseekBar.getProgress());
-	        	//�ύ����
-	        	editor.commit();
-	        } else {   
-	        }  
-	    }  
-	    @Override  
-	    public void onProgressChanged(SeekBar seekBar, int progress,  
-	            boolean fromUser) {  
-	        mtextView.setText(progress + "/" + seekBar.getMax()); 
-	    }  
-		@Override  
-	    public void onStartTrackingTouch(SeekBar seekBar) {  
-	        // TODO Auto-generated method stub  
-	  
-	    }  
-	  
-	    @Override  
-	    public void onStopTrackingTouch(SeekBar seekBar) {  
-	        // TODO Auto-generated method stub  
-	  
-	    }
+
+	@Override
+	protected void onBindDialogView(View view) {
+		// TODO Auto-generated method stub
+		super.onBindDialogView(view);
+		mSharedPrefs = this.getSharedPreferences();
+		seekvalue = mSharedPrefs.getInt(pre_ShakeThreshold_on_shake_value,
+				defaultvalue);
+		mseekBar = (SeekBar) view.findViewById(R.id.seekBar);
+		mtextView = (TextView) view.findViewById(R.id.textView);
+		mseekBar.setOnSeekBarChangeListener(this);
+		this.mseekBar.setProgress(this.seekvalue);
+		mtextView.setText(seekvalue + "/" + mseekBar.getMax());
+	}
+
+	@Override
+	protected void onDialogClosed(boolean positiveResult) {
+		// TODO Auto-generated method stub
+		if (positiveResult) {
+			SharedPreferences.Editor editor = mSharedPrefs.edit();
+			editor.putInt(pre_ShakeThreshold_on_shake_value,
+					mseekBar.getProgress());
+			editor.commit();
+		} else {
+		}
+	}
+
+	@Override
+	public void onProgressChanged(SeekBar seekBar, int progress,
+			boolean fromUser) {
+		mtextView.setText(progress + "/" + seekBar.getMax());
+	}
+
+	@Override
+	public void onStartTrackingTouch(SeekBar seekBar) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onStopTrackingTouch(SeekBar seekBar) {
+		// TODO Auto-generated method stub
+
+	}
 
 }
