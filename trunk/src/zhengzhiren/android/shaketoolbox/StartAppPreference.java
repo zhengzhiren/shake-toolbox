00e09fb13540771ede0626d28aa53a4dfa9e3b83
tilltheendwjx@gmail.com <tilltheendwjx@gmail.com@bfed1615-f5b6-2cc9-921f-102c86f96327>
2012-05-09 16:06:03 +0000

    加载应用程序列表缓冲界面和加载优化

--- trunk/src/zhengzhiren/android/shaketoolbox/StartAppPreference.java
+++ trunk/src/zhengzhiren/android/shaketoolbox/StartAppPreference.java
@@ -5,14 +5,16 @@ package zhengzhiren.android.shaketoolbox;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Message;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
@@ -21,7 +23,6 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.BaseAdapter;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.RadioButton;
@@ -32,18 +33,22 @@ import android.widget.TextView;
  * 
  */
 public class StartAppPreference extends PreferenceActivity {
-	
-	public static final String APP_LAUNCH_INTENT_NAME="app_launch_intent_name";
+
+	public static final String APP_LAUNCH_INTENT_NAME = "app_launch_intent_name";
+	private static final int MSG_UPDATE_LIST = 0;
 	private ListView mListView;
 	private List<Programe> mListPrograme;
 	private AppListAdapter mAppListAdapter;
-	private Programe mCurrentPrograme=null;
+	private Programe mCurrentPrograme = null;
+	private ProgressDialog dialog;
+
 	/**
 	 * 
 	 */
 	public StartAppPreference() {
 
 	}
+
 	@Override
 	protected void onResume() {
 		super.onResume();
@@ -52,120 +57,188 @@ public class StartAppPreference extends PreferenceActivity {
 	@Override
 	protected void onStart() {
 		super.onStart();
-		
+
 	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		this.setContentView(R.layout.start_app_preferences);
-		mListView = (ListView) findViewById(android.R.id.list);
-		getAppList();
-		mAppListAdapter = new AppListAdapter(mListPrograme, this);
-		mAppListAdapter.setCurSelectPosition(getPos(getApp()));
-		mListView.setAdapter(mAppListAdapter);
 		
+		mListView = (ListView) findViewById(android.R.id.list);
+		mListPrograme=new ArrayList<Programe>();
+		mAppListAdapter = new AppListAdapter(mListPrograme, this);
+		
+		
+		
+        dialog = new ProgressDialog(this);   
+        dialog.setTitle("应用程序列表");  
+        dialog.setMessage("正在加载数据中...");  
+        dialog.setCancelable(false);  
+        dialog.show();  
+          
+        mThreadLoadApps.start(); 
+
 		mListView.setOnItemClickListener(new OnItemClickListener() {
-			/**update list UI when item on click*/  
-	        class UpdateListStaus implements Runnable{  
-	            private View mView;   
-	              
-	            public UpdateListStaus(View view){  
-	                this.mView  = view;  
-	            }  
-	              
-	            public void run(){  
-	            	if(mAppListAdapter.getSelectHolder() != null){  
-	            		mAppListAdapter.getSelectHolder().radiobutton.setChecked(false);
-	                } 
-	            	ViewHolder tmp = new ViewHolder();  
-	                tmp.radiobutton= (RadioButton)mView.findViewById(R.id.app_rbtn); 
-	                tmp.imgage = (ImageView) mView
-							.findViewById(R.id.app_image);
-	                tmp.text = (TextView) mView
-							.findViewById(R.id.app_text);
-	                tmp.radiobutton.setChecked(true) ;
-	                mAppListAdapter.setSelectHolder(tmp); 
-	            }  
-	        }  
+			class UpdateListStaus implements Runnable {
+				private View mView;
+
+				public UpdateListStaus(View view) {
+					this.mView = view;
+				}
+
+				public void run() {
+					if (mAppListAdapter.getSelectHolder() != null) {
+						mAppListAdapter.getSelectHolder().radiobutton
+								.setChecked(false);
+					}
+					ViewHolder tmp = new ViewHolder();
+					tmp.radiobutton = (RadioButton) mView
+							.findViewById(R.id.app_rbtn);
+					tmp.imgage = (ImageView) mView.findViewById(R.id.app_image);
+					tmp.text = (TextView) mView.findViewById(R.id.app_text);
+					tmp.radiobutton.setChecked(true);
+					mAppListAdapter.setSelectHolder(tmp);
+				}
+			}
+
 			@Override
 			public void onItemClick(AdapterView<?> adapterView, View view,
 					int pos, long id) {
 				mAppListAdapter.setCurSelectPosition(pos);
-				new Handler().post(new UpdateListStaus(view)); 
-				mCurrentPrograme=mListPrograme.get(pos);
-				
-			}
-		});
-		findViewById(R.id.start_app_cancel).setOnClickListener(new View.OnClickListener() {
+				new Handler().post(new UpdateListStaus(view));
+				mCurrentPrograme = mListPrograme.get(pos);
 
-			@Override
-			public void onClick(View view) {
-				finish();
 			}
 		});
-		findViewById(R.id.start_app_save).setOnClickListener(new View.OnClickListener() {
+		findViewById(R.id.start_app_cancel).setOnClickListener(
+				new View.OnClickListener() {
 
-			@Override
-			public void onClick(View view) {
-				saveApp();
-				finish();
-			}
-		});
+					@Override
+					public void onClick(View view) {
+						finish();
+					}
+				});
+		findViewById(R.id.start_app_save).setOnClickListener(
+				new View.OnClickListener() {
+
+					@Override
+					public void onClick(View view) {
+						saveApp();
+						finish();
+					}
+				});
+		
+		
+
 	}
-	private String getApp()
-	{
+	private Handler handler = new Handler(){  
+        @Override  
+        public void handleMessage(Message msg) {  
+            // TODO Auto-generated method stub  
+            switch (msg.what) {  
+            case MSG_UPDATE_LIST:  
+                // 更新应用程序列表  
+            	mListView.setAdapter(mAppListAdapter);
+                break;  
+            } 
+            super.handleMessage(msg);  
+        }  
+    };  
+	private Thread mThreadLoadApps = new Thread(){  
+        @Override  
+        public void run() {  
+        	getAppList();
+            dialog.dismiss();  
+            mAppListAdapter.setCurSelectPosition(getPos(getApp()));
+        }  
+          
+    };
+	private String getApp() {
 		SharedPreferences sp = PreferenceManager
 				.getDefaultSharedPreferences(this);
 		return sp.getString(APP_LAUNCH_INTENT_NAME, null);
 	}
-	private void saveApp()
-	{
-		if(mCurrentPrograme==null)
-		{
+
+	private void saveApp() {
+		if (mCurrentPrograme == null) {
 			return;
 		}
-		if(mCurrentPrograme.packname.isEmpty()||mCurrentPrograme.packname==null)
-		{
+		if (mCurrentPrograme.getPackgeName().isEmpty()
+				|| mCurrentPrograme.getPackgeName() == null) {
 			return;
 		}
 		SharedPreferences sp = PreferenceManager
 				.getDefaultSharedPreferences(this);
 		SharedPreferences.Editor editor = sp.edit();
-		editor.putString(APP_LAUNCH_INTENT_NAME,mCurrentPrograme.packname);
+		editor.putString(APP_LAUNCH_INTENT_NAME, mCurrentPrograme.getPackgeName());
 		editor.apply();
 	}
+	
+	private void getAppList() {
+		PackageManager pm;
+		pm = getPackageManager();
+		List<ApplicationInfo> appList = pm
+				.getInstalledApplications(PackageManager.GET_UNINSTALLED_PACKAGES);
+		mListPrograme = new ArrayList<Programe>();
+		for (ApplicationInfo app : appList) {
+			if (app.processName.equals("system") || (app == null)) {
+				continue;
+			}
+			Programe pr = new Programe();
+			pr.setIcon(app.loadIcon(pm));
+			pr.setName(app.loadLabel(pm).toString());
+			pr.setPackgeName(app.packageName.toString());
+			mListPrograme.add(pr);
+			handler.sendEmptyMessage(MSG_UPDATE_LIST);
+		} 
+	}
+
+	public int getPos(String packname) {
+		if (packname == null || packname.isEmpty()) {
+			return -1;
+		}
+		int i = 0;
+		for (Programe appinfo : mListPrograme) {
+			if (packname.equals(appinfo.packname)) {
+				return i;
+			}
+			i++;
+		}
+		return -1;
+	}
+	
 	public class AppListAdapter extends BaseAdapter {
-		List<Programe> list = new ArrayList<Programe>();
 		LayoutInflater la;
 		Context context;
-		/**on selected item*/  
-        private ViewHolder mSelectHolder;  
-        /**on selected item posion*/  
+		private ViewHolder mSelectHolder;
 		private int mCurSelectPosition = -1;
 
 		public AppListAdapter(List<Programe> list, Context context) {
-			this.list = list;
 			this.context = context;
 			la = LayoutInflater.from(context);
 		}
-		public void setCurSelectPosition(int curSelectPosition) {  
-            mCurSelectPosition = curSelectPosition;  
-        }  
-        public ViewHolder getSelectHolder() {  
-            return mSelectHolder;  
-        }  
-        public void setSelectHolder(ViewHolder selectHolder) {  
-            this.mSelectHolder = selectHolder;  
-        }  
+
+		public void setCurSelectPosition(int curSelectPosition) {
+			mCurSelectPosition = curSelectPosition;
+		}
+
+		public ViewHolder getSelectHolder() {
+			return mSelectHolder;
+		}
+
+		public void setSelectHolder(ViewHolder selectHolder) {
+			this.mSelectHolder = selectHolder;
+		}
+
 		@Override
 		public int getCount() {
-			return list.size();
+			return mListPrograme.size();
 		}
 
 		@Override
 		public Object getItem(int position) {
-			return list.get(position);
+			return mListPrograme.get(position);
 		}
 
 		@Override
@@ -176,7 +249,7 @@ public class StartAppPreference extends PreferenceActivity {
 		@Override
 		public View getView(int position, View convertView, ViewGroup parent) {
 			ViewHolder holder;
-			final Programe pr = (Programe) list.get(position);
+			final Programe pr = (Programe) mListPrograme.get(position);
 			if (convertView == null) {
 				convertView = la.inflate(R.layout.list_item_apps, null);
 				holder = new ViewHolder();
@@ -186,21 +259,17 @@ public class StartAppPreference extends PreferenceActivity {
 						.findViewById(R.id.app_text);
 				holder.radiobutton = (RadioButton) convertView
 						.findViewById(R.id.app_rbtn);
-				
+
 				convertView.setTag(holder);
 			} else {
 				holder = (ViewHolder) convertView.getTag();
 			}
-			// 设置图标
 			holder.imgage.setImageDrawable(pr.getIcon());
-			// 设置程序名
 			holder.text.setText(pr.getName());
-			if(position==mCurSelectPosition)
-			{
+			if (position == mCurSelectPosition) {
 				setSelectHolder(holder);
 				holder.radiobutton.setChecked(true);
-			}else
-			{
+			} else {
 				holder.radiobutton.setChecked(false);
 			}
 			return convertView;
@@ -212,44 +281,20 @@ public class StartAppPreference extends PreferenceActivity {
 		ImageView imgage;
 		TextView text;
 	}
-	private void getAppList()
-	{
-		PackageManager pm;
-		pm = getPackageManager();
-		List<ApplicationInfo> appList = pm.getInstalledApplications(PackageManager.GET_UNINSTALLED_PACKAGES);
-		mListPrograme= new ArrayList<Programe>();
-		for (ApplicationInfo app : appList) {
-			// 这里主要是过滤系统的应用和电话应用，当然你也可以把它注释掉。
-			if (app.processName.equals("system")
-					|| (app == null)) {
-				continue;
-			}
-			Programe pr = new Programe();
-			pr.setIcon(app.loadIcon(pm));
-			pr.setName(app.loadLabel(pm).toString());
-			pr.setPackgeName(app.packageName.toString());
-			mListPrograme.add(pr);
+	class Programe {
+		
+		public Programe()
+		{
+			this.icon=null;
+			this.name=null;
+			this.packname=null;
+			
 		}
-	}
-	public int getPos(String packname) {
-		if (packname == null||packname.isEmpty()) {
-			return -1;
-		}
-		int i=0;
-		for (Programe appinfo : mListPrograme) {
-			if (packname.equals(appinfo.packname)) {
-				return i;
-			}
-			i++;
-		}
-		return -1;
-	}
-	public class Programe {
-		// 图标
+		
 		private Drawable icon;
-		// 程序名
 		private String name;
 		private String packname;
+
 		public Drawable getIcon() {
 			return icon;
 		}
@@ -265,6 +310,7 @@ public class StartAppPreference extends PreferenceActivity {
 		public void setName(String name) {
 			this.name = name;
 		}
+
 		public String getPackgeName() {
 			return packname;
 		}
